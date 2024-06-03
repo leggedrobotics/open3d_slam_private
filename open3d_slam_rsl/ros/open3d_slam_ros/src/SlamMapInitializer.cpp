4967d01daa73cfb5532f6bab85faab8730c3fd33
Benjamin Krummenacher <benjamin.krummenacher@outlook.com>
2024-06-03 23:12:53 +0200

    also allow loading maps with absolute paths

--- open3d_slam_rsl/ros/open3d_slam_ros/src/SlamMapInitializer.cpp
+++ open3d_slam_rsl/ros/open3d_slam_ros/src/SlamMapInitializer.cpp
@@ -50,11 +50,17 @@ bool SlamMapInitializer::initSlamCallback(std_srvs::Trigger::Request& req, std_s
 void SlamMapInitializer::initialize(const MapInitializingParameters& params) {
   mapInitializerParams_ = params;
 
-  PointCloud raw_map;
-  std::string pcdFile = ros::package::getPath(mapInitializerParams_.pcdFilePackage_) + mapInitializerParams_.pcdFilePath_;
+  std::string pcdFile;
+  if (!mapInitializerParams_.pcdFilePath_.empty() && mapInitializerParams_.pcdFilePath_[0] == '/') {
+    pcdFile = mapInitializerParams_.pcdFilePath_;
+  }
+  else {
+    pcdFile = ros::package::getPath(mapInitializerParams_.pcdFilePackage_) + mapInitializerParams_.pcdFilePath_;
+  }
 
   initialized_.store(false);
 
+  PointCloud raw_map;
   std::cout << "Loading pointloud from: " << pcdFile << "\n";
   if (!open3d::io::ReadPointCloud(pcdFile, raw_map)) {
     std::cerr << "[Error] Initialization pointcloud not loaded" << std::endl;
